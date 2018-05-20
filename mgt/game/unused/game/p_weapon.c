3357be04f515eaa6cacaf2f9a388e60015be14d7
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-05-20 13:58:00 +0000

    'mgt/unused/' -> 'mgt/game/unused/' [ci skip]

---
+++ mgt/game/unused/game/p_weapon.c
@@ -0,0 +1,115 @@
+/*
+Copyright (C) 1997-2001 Id Software, Inc.
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+// g_weapon.c
+
+#include "g_local.h"
+#include "m_player.h"
+
+
+static bool	is_quad;
+static byte		is_silenced;
+
+
+void weapon_grenade_fire (edict_t *ent, qboolean held);
+
+
+static void P_ProjectSource (gclient_t *client, vec3_t point, vec3_t distance, vec3_t forward, vec3_t right, vec3_t result)
+{
+	vec3_t	_distance;
+
+	VectorCopy (distance, _distance);
+	if (client->pers.hand == LEFT_HANDED)
+		_distance[1] *= -1;
+	else if (client->pers.hand == CENTER_HANDED)
+		_distance[1] = 0;
+	G_ProjectSource (point, _distance, forward, right, result);
+}
+
+
+/*
+===============
+PlayerNoise
+
+Each player can have two noise objects associated with it:
+a personal noise (jumping, pain, weapon firing), and a weapon
+target noise (bullet wall impacts)
+
+Monsters that don't directly see the player can move
+to a noise in hopes of seeing the player from there.
+===============
+*/
+void PlayerNoise(edict_t *who, vec3_t where, int type)
+{
+	edict_t		*noise;
+
+	if (type == PNOISE_WEAPON)
+	{
+		if (who->client->silencer_shots)
+		{
+			who->client->silencer_shots--;
+			return;
+		}
+	}
+
+	if (deathmatch->value)
+		return;
+
+	if (who->flags & FL_NOTARGET)
+		return;
+
+
+	if (!who->mynoise)
+	{
+		noise = G_Spawn();
+		noise->classname = "player_noise";
+		VectorSet (noise->mins, -8, -8, -8);
+		VectorSet (noise->maxs, 8, 8, 8);
+		noise->owner = who;
+		noise->svflags = SVF_NOCLIENT;
+		who->mynoise = noise;
+
+		noise = G_Spawn();
+		noise->classname = "player_noise";
+		VectorSet (noise->mins, -8, -8, -8);
+		VectorSet (noise->maxs, 8, 8, 8);
+		noise->owner = who;
+		noise->svflags = SVF_NOCLIENT;
+		who->mynoise2 = noise;
+	}
+
+	if (type == PNOISE_SELF || type == PNOISE_WEAPON)
+	{
+		noise = who->mynoise;
+		level.sound_entity = noise;
+		level.sound_entity_framenum = level.framenum;
+	}
+	else // type == PNOISE_IMPACT
+	{
+		noise = who->mynoise2;
+		level.sound2_entity = noise;
+		level.sound2_entity_framenum = level.framenum;
+	}
+
+	VectorCopy (where, noise->s.origin);
+	VectorSubtract (where, noise->maxs, noise->absmin);
+	VectorAdd (where, noise->maxs, noise->absmax);
+	noise->teleport_time = level.time;
+	gi.linkentity (noise);
+}
