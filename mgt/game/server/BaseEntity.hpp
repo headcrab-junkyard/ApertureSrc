df0045e3f58d5575515e821809fec598c779cab1
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-06-02 10:22:57 +0000

    [GameDLL] Derive base entity class from IGameEntity interface [ci skip]

--- mgt/game/server/BaseEntity.hpp
+++ mgt/game/server/BaseEntity.hpp
@@ -1,22 +1,21 @@
 /// @file
-/// @brief base entity class
+/// @brief base game entity class
 
 #pragma once
 
+#include "game/server/IGameEntity.hpp"
+
 typedef struct entvars_s entvars_t;
 
-class CBaseEntity
+class CBaseEntity : public IGameEntity
 {
 public:
 	CBaseEntity(entvars_t *vars) = default;
 	virtual ~CBaseEntity() = default;
 	
-	virtual void OnSpawn(){}
-	virtual void OnTouch(const CBaseEntity &apOther){}
-	virtual void Use(const CBaseEntity &apOther){}
-	
-	void SetModel(const char *name);
-	void SetSize(float *mins, float *maxs);
+	virtual void OnSpawn() override {}
+	virtual void OnTouch(const IEntity &apOther) override {}
+	virtual void OnUse(const IEntity &apOther) override {}
 	
 	const char *GetClassName() const;
 	float GetHealth() const;
