782ea3b9812528144e90bb8d500105942753b855
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-06-04 15:18:20 +0000

    Add resource manager interface [ci skip]

---
+++ mgt/common/engine/IResourceManager.hpp
@@ -0,0 +1,46 @@
+
+/*
+ * This file is part of Magenta Engine
+ *
+ * Copyright (C) 2018 BlackPhrase
+ *
+ * Magenta Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Magenta Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+
+#pragma once
+
+#include "CommonTypes.hpp"
+
+constexpr auto MGT_CLIENTRESOURCEMANAGER_INTERFACE_VERSION{"MGTClientResourceManager001Alpha"};
+constexpr auto MGT_SERVERRESOURCEMANAGER_INTERFACE_VERSION{"MGTServerResourceManager001Alpha"};
+
+interface IResourceManager : public IBaseInterface
+{
+	///
+	virtual int PrecacheModel(const char *asName) = 0;
+	
+	///
+	virtual int PrecacheSound(const char *asName) = 0;
+	
+	///
+	virtual int PrecacheGeneric(const char *asName) = 0;
+	
+	///
+	virtual int GetModelIndex(const char *asName) const = 0;
+	
+	///
+	virtual int GetDecalIndex(const char *asName) const = 0;
+};
