b92b296cfcfe8a7f05c77688558fd6688f97e5e7
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-06-09 13:15:35 +0000

    [GameDLL] Update game interface implementation [ci skip]

--- mgt/game/server/Game.cpp
+++ mgt/game/server/Game.cpp
@@ -1,6 +1,26 @@
+/*
+ * This file is part of Magenta Engine
+ *
+ * Copyright (C) 2015-2018 BlackPhrase
+ *
+ * Magenta Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Magenta Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /// @file
 
 #include "Game.hpp"
+#include "SystemEventListener.hpp"
 
 IConsoleManager *gpConsoleManager{nullptr};
 
@@ -24,29 +44,59 @@ CGame::~CGame() = default;
 
 bool CGame::Init(CreateInterfaceFn afnEngineFactory)
 {
-	// Put your init routines here...
+	// NOTE: Put your init routines here...
 	
-	gpConsoleManager = (IConsoleManager*)afnEngineFactory(MGT_CONSOLEMANAGER_INTERFACE_VERSION, nullptr);
+	mpSystem = (ISystem*)afnEngineFactory(MGT_SYSTEM_INTERFACE_VERSION, nullptr);
 	
-	if(!gpConsoleManager)
+	gpCmdRegistry = (ICmdRegistry*)afnEngineFactory(MGT_CMDREGISTRY_INTERFACE_VERSION, nullptr);
+	
+	if(!mpSystem || !gpCmdRegistry)
 		return false;
 	
-	CConCmd Cmd_DoABarrelRoll("barrelroll", DoABarrelRoll_f, FCVAR_EXTDLL, "Does a barrel roll (kinda)");
+	// Add our system event listener here
+	mpSystemEventListener = std::make_unique<CSystemEventListener>();
+	mpSystem->AddEventListener(mpSystemEventListener.get());
 	
-	if(!gpConsoleManager->RegisterCmd(Cmd_DoABarrelRoll))
+	// Add entity event listener
+	//mpEntityEventListener = std::make_unique<CEntityEventListener>();
+	//mpSystem->AddEventListener(mpEntityEventListener.get());
+	
+	// Add game client event listener
+	//mpGameClientEventListener = std::make_unique<CGameClientEventListener>();
+	//mpServer->AddClientEventListener(mpGameClientEventListener.get());
+	
+	//CConCmd Cmd_DoABarrelRoll("barrelroll", DoABarrelRoll_f, FCVAR_EXTDLL, "Does a barrel roll (kinda)");
+	//if(!gpCmdRegistry->Register(Cmd_DoABarrelRoll))
+	if(!gpCmdRegistry->Add("barrelroll", DoABarrelRoll_f, "Does a barrel roll (kinda)"))
 		return false;
 	
 	// Something game-specific...
+	
+	GameInit();
+	RegisterEncoders();
+	
+	//mpServer->SetLagCompensationActive(true); // TODO
+	
+	//mpWorld->SetDefaultHulls(); // Setup default player hulls // TODO
 	
 	return true;
 };
 
 void CGame::Shutdown()
 {
-	// Put your shutdown routines here...
+	// NOTE: Put your shutdown routines here...
+	
+	//mpServer->RemoveClientEventListener(mpGameClientEventListener.get());
+	//mpWorld->RemoveEntityEventListener(mpEntityEventListener.get());
+	mpSystem->RemoveEventListener(mpSystemEventListener.get());
 };
 
-void CGame::Update()
+void CGame::Update() // Frame
 {
 	// Per-frame logic update
+};
+
+const char *CGame::GetDescription() const
+{
+	return "Template";
 };
--- mgt/game/server/Game.hpp
+++ mgt/game/server/Game.hpp
@@ -1,8 +1,31 @@
+/*
+ * This file is part of Magenta Engine
+ *
+ * Copyright (C) 2016-2018 BlackPhrase
+ *
+ * Magenta Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Magenta Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /// @file
 
 #pragma once
 
+#include <memory>
 #include "game/server/IGame.hpp"
+
+interface ISystem;
+class CSystemEventListener;
 
 class CGame final : public IGame
 {
@@ -10,9 +33,27 @@ public:
 	CGame() = default;
 	~CGame() = default;
 	
-	bool Init(CreateInterfaceFn afnEngineFactory) override;
-	void Shutdown() override;
+	bool Init(CreateInterfaceFn afnEngineFactory) override; // DLLInit
+	void Shutdown() override; // DLLShutdown
 	
-	void Update() override;
+	//void PostInit();
+	
+	//bool GameInit();
+	//void GameShutdown();
+	
+	//void OnServerActivate(edict_t *apEdictList, uint anEdictCount, uint anMaxPlayers);
+	//void OnServerDeactivate();
+	
+	//bool ShouldHideServer();
+	
+	void Update() override; // Frame
+	
+	const char *GetDescription() const override;
 private:
+	void GameInit();
+	void RegisterEncoders();
+	
+	std::unique_ptr<CSystemEventListener> mpSystemEventListener;
+	
+	ISystem *mpSystem{nullptr};
 };
