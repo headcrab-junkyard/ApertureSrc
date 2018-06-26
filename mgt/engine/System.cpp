5674635b641aec82aa00439839337fa55eed58a6
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-06-26 13:41:31 +0000

    [Engine] Update system interface implementation [ci skip]

--- mgt/engine/System.cpp
+++ mgt/engine/System.cpp
@@ -1,7 +1,27 @@
+/*
+*	This file is part of Magenta Engine
+*
+*	Copyright (C) 2018 BlackPhrase
+*
+*	Magenta Engine is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*
+*	Magenta Engine is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License
+*	along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /// @file
 
 #include "quakedef.h"
 #include "System.hpp"
+#include "SystemEventDispatcher.hpp"
 #include "interface.h"
 
 CSystem gSystem;
@@ -9,8 +29,22 @@ CSystem gSystem;
 
 EXPOSE_SINGLE_INTERFACE_GLOBALVAR(CSystem, ISystem, MGT_SYSTEM_INTERFACE_VERSION, gSystem);
 
-CSystem::CSystem() = default;
+CSystem::CSystem()
+{
+	mpEventDispatcher = std::make_unique<CSystemEventDispatcher>();
+};
+
 CSystem::~CSystem() = default;
+
+void CSystem::AddEventListener(ISystemEventListener *apListener)
+{
+	mpEventDispatcher->AddListener(apListener);
+};
+
+void CSystem::RemoveEventListener(ISystemEventListener *apListener)
+{
+	mpEventDispatcher->RemoveListener(apListener);
+};
 
 void CSystem::Error(const char *fmt, ...)
 {
@@ -21,6 +55,8 @@ void CSystem::Error(const char *fmt, ...)
 	vsprintf(text, fmt, argptr);
 	va_end(argptr);
 
+	mpEventDispatcher->DispatchError(text);
+	
 	Sys_Error(text);
 };
 
--- mgt/engine/System.hpp
+++ mgt/engine/System.hpp
@@ -1,8 +1,30 @@
+/*
+*	This file is part of Magenta Engine
+*
+*	Copyright (C) 2018 BlackPhrase
+*
+*	Magenta Engine is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*
+*	Magenta Engine is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License
+*	along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /// @file
 
 #pragma once
 
+#include <memory>
 #include "engine/ISystem.hpp"
+
+class CSystemEventDispatcher;
 
 class CSystem final : public ISystem
 {
@@ -10,9 +32,14 @@ public:
 	CSystem();
 	~CSystem();
 
+	void AddEventListener(ISystemEventListener *apListener) override;
+	void RemoveEventListener(ISystemEventListener *apListener) override;
+	
 	void Error(const char *fmt, ...) override;
 
 	double GetFloatTime() const override;
+private:
+	std::unique_ptr<CSystemEventDispatcher> mpEventDispatcher;
 };
 
 //extern CSystem *gpSystem;
