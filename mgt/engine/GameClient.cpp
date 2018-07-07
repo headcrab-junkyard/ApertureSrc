bd1892d7878c9b4b8dd3e25c3cf8b8eb9165e602
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-07-07 17:28:32 +0000

    [Engine] Update game client interface implementation

---
+++ mgt/engine/GameClient.cpp
@@ -0,0 +1,135 @@
+/*
+ * This file is part of Magenta Engine
+ *
+ * Copyright (C) 2018 BlackPhrase
+ *
+ * Magenta Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Magenta Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+
+#include "quakedef.h"
+#include "GameClient.hpp"
+
+CGameClient::CGameClient() = default;
+CGameClient::~CGameClient() = default;
+
+/*
+=================
+PF_sprint
+
+single print to a specific client
+
+sprint(clientent, value)
+=================
+*/
+void CGameClient::Print(const char *s)
+{
+	auto entnum{NUM_FOR_EDICT(mpData->edict)};
+
+	if(entnum < 1 || entnum > svs.maxclients)
+	{
+		Con_Printf("tried to sprint to a non-client\n");
+		return;
+	};
+
+	mpData = &svs.clients[entnum - 1];
+
+	MSG_WriteChar(&mpData->netchan.message, svc_print);
+	MSG_WriteString(&mpData->netchan.message, s);
+};
+
+/*
+=================
+PF_centerprint
+
+single print to a specific client
+
+centerprint(clientent, value)
+=================
+*/
+void CGameClient::CenterPrint(const char *s)
+{
+	auto entnum{NUM_FOR_EDICT(mpData->edict)};
+
+	if(entnum < 1 || entnum > svs.maxclients)
+	{
+		Con_Printf("tried to sprint to a non-client\n");
+		return;
+	};
+
+	mpData = &svs.clients[entnum - 1]; // TODO: WUT?
+
+	MSG_WriteChar(&mpData->netchan.message, svc_centerprint);
+	MSG_WriteString(&mpData->netchan.message, s);
+};
+
+/*
+=================
+PF_stuffcmd
+
+Sends text over to the client's execution buffer
+
+stuffcmd (clientent, value)
+=================
+*/
+void CGameClient::SendStuffCmd(const char *str)
+{
+	auto entnum{NUM_FOR_EDICT(mpData->edict)};
+	
+	if(entnum < 1 || entnum > svs.maxclients)
+		Host_Error("Parm 0 not a client"); // TODO
+
+	Host_ClientCommands(mpData, "%s", str); // TODO: Host_SendClientCmd
+};
+
+void CGameClient::Printf(PrintType aeType, const char *asMsg, ...)
+{
+	// TODO
+};
+
+void CGameClient::SendCommand(const char *asText, ...)
+{
+	// TODO
+};
+
+void CGameClient::Disconnect(const char *asReason, ...)
+{
+	// TODO
+};
+
+int CGameClient::GetUserID() const
+{
+	return -1; // TODO
+};
+
+uint CGameClient::GetWONID() const
+{
+	return 0; // TODO
+};
+
+const char *CGameClient::GetAuthID() const
+{
+	return ""; // TODO
+};
+
+void CGameClient::QueryCvarValue(const char *asCvarName)
+{
+	// TODO
+};
+
+void CGameClient::QueryCvarValueEx(const char *asCvarName, int anRequestID)
+{
+	// TODO
+};
--- mgt/engine/GameClient.hpp
+++ mgt/engine/GameClient.hpp
@@ -1,3 +1,22 @@
+/*
+ * This file is part of Magenta Engine
+ *
+ * Copyright (C) 2018 BlackPhrase
+ *
+ * Magenta Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Magenta Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /// @file
 
 #pragma once
@@ -10,9 +29,29 @@ public:
 	CGameClient();
 	~CGameClient();
 
+	void Print(const char *asMsg); // TODO
+	void CenterPrint(const char *asMsg); // TODO
+	void SendStuffCmd(const char *asCmd); // TODO
+	
+	void Printf(PrintType aeType, const char *asMsg, ...) override;
+	
+	void SendCommand(const char *asText, ...) override;
+	
 	void Disconnect(const char *asReason, ...) override;
 
-	void Printf(const char *asMsg, ...) override;
-
+	int GetUserID() const override;
+	uint GetWONID() const override;
+	const char *GetAuthID() const override;
+	
+	void QueryCvarValue(const char *asCvarName) override;
+	void QueryCvarValueEx(const char *asCvarName, int anRequestID) override;
+	
+	void SetViewEntity(const IEntity *apViewEnt) override; //{mpViewEnt = apViewEnt;} // TODO: const-correctness
+	
+	IEntity *GetEntity() const override {return mpEntity;}
 private:
+	IEntity *mpEntity{nullptr};
+	IEntity *mpViewEnt{nullptr};
+	
+	client_t *mpData{nullptr}; // TODO: temp?
 };
