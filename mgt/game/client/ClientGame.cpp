bd2bffcb6487486f72e0303894fa5ace5808de62
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-08-02 13:24:02 +0000

    [ClientDLL] Update IClientGame interface implementation

--- mgt/game/client/ClientGame.cpp
+++ mgt/game/client/ClientGame.cpp
@@ -1,20 +1,21 @@
 /*
- *	This file is part of Magenta Engine
- *	Copyright (C) 2017-2018 BlackPhrase
+ * This file is part of Magenta Engine
  *
- *	Magenta Engine is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
+ * Copyright (C) 2017-2018 BlackPhrase
  *
- *	Magenta Engine is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *	GNU General Public License for more details.
+ * Magenta Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
- */
+ * Magenta Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /// @file
 
@@ -22,6 +23,8 @@
 #include "memory/IMemory.hpp"
 #include "console/IConsole.hpp"
 #include "engine/IEngineSound.hpp"
+#include "input/IInput.hpp"
+#include "InputEventListener.hpp"
 
 EXPOSE_SINGLE_INTERFACE(CClientGame, IClientGame, MGT_CLIENTGAME_INTERFACE_VERSION);
 
@@ -30,26 +33,31 @@ CClientGame::~CClientGame() = default;
 
 bool CClientGame::Init(CreateInterfaceFn afnEngineFactory)
 {
-	mpMemory = (IMemory*)afnEngineFactory(MGT_MEMORY_INTERFACE_VERSION, nullptr);
+	// NOTE: Put your init routines here...
 	
-	if(!mpMemory)
-		return false;
+	mpMemory = (IMemory*)afnEngineFactory(MGT_MEMORY_INTERFACE_VERSION, nullptr);
 	
 	mpConsole = (IConsole*)afnEngineFactory(MGT_CONSOLE_INTERFACE_VERSION, nullptr);
 	
-	if(!mpConsole)
-		return false;
-	
 	mpSound = (IEngineSound*)afnEngineFactory(MGT_ENGINESOUNDCLIENT_INTERFACE_VERSION, nullptr);
 	
-	if(!mpSound)
+	mpInput = (IInput*)afnEngineFactory(MGT_INPUT_INTERFACE_VERSION, nullptr);
+	
+	if(!mpMemory || !mpConsole || !mpSound || !mpInput)
 		return false;
+	
+	// Plug our input events listener in
+	mpInputEventListener = std::make_unique<CInputEventListener>();
+	mpInput->AddEventListener(mpInputEventListener.get());
 	
 	return true;
 };
 
 void CClientGame::Shutdown()
 {
+	// NOTE: Put your shutdown routines here...
+	
+	mpInput->RemoveEventListener(mpInputEventListener.get());
 };
 
 void CClientGame::Frame()
--- mgt/game/client/ClientGame.hpp
+++ mgt/game/client/ClientGame.hpp
@@ -1,30 +1,34 @@
 /*
- *	This file is part of Magenta Engine
- *	Copyright (C) 2017-2018 BlackPhrase
+ * This file is part of Magenta Engine
  *
- *	Magenta Engine is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
+ * Copyright (C) 2017-2018 BlackPhrase
  *
- *	Magenta Engine is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *	GNU General Public License for more details.
+ * Magenta Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
- */
+ * Magenta Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /// @file
 
 #pragma once
 
+#include <memory>
 #include "game/client/IClientGame.hpp"
 
 struct IMemory;
 struct IConsole;
 struct IEngineSound;
+struct IInput;
+class CInputEventListener;
 
 class CClientGame : public IClientGame
 {
@@ -32,12 +36,15 @@ public:
 	CClientGame();
 	~CClientGame();
 	
-	bool Init(CreateInterfaceFn afnEngineFactory);
-	void Shutdown();
+	bool Init(CreateInterfaceFn afnEngineFactory) override;
+	void Shutdown() override;
 	
-	void Frame();
+	void Frame() override;
 private:
+	std::unique_ptr<CInputEventListener> mpInputEventListener;
+	
 	IMemory *mpMemory{nullptr};
 	IConsole *mpConsole{nullptr};
 	IEngineSound *mpSound{nullptr};
+	IInput *mpInput{nullptr};
 };
