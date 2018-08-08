8790bf8f6d8c319708c60bb9670c7d4a7e158e12
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-08-08 12:37:48 +0000

    [Engine] Restore script manager code (FIXME)

---
+++ mgt/engine/fixme/ScriptManager.hpp
@@ -0,0 +1,43 @@
+/*
+ * This file is part of Magenta Engine
+ *
+ * Copyright (C) 2016-2018 BlackPhrase
+ *
+ * Magenta Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Magenta Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+
+#pragma once
+
+#include <vector>
+
+interface IScript;
+using tScriptVec = std::vector<IScript*>;
+
+class CScriptManager final
+{
+public:
+	CScriptManager();
+	~CScriptManager();
+	
+	void LoadScript(const char *asName);
+	void UnloadScript(const char *asName);
+	
+	void LoadScriptsFromFolder(const char *asPath);
+	
+	void UnloadAllScripts();
+private:
+	tScriptVec mvScripts;
+};
