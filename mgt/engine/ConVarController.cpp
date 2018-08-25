f50c380a9a955b7aae04ae7b21a349d5c3ea04d2
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-08-25 14:41:27 +0000

    [Engine] Add convar controller interface implementation

---
+++ mgt/engine/ConVarController.cpp
@@ -0,0 +1,46 @@
+/*
+ * This file is part of Magenta Engine
+ *
+ * Copyright (C) 2018 BlackPhrase
+ *
+ * Magenta Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Magenta Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+
+#include "quakedef.h"
+#include "ConVarController.hpp"
+
+CConVarController::CConVarController() = default;
+CConVarController::~CConVarController() = default;
+
+void CConVarController::SetString(const char *asName, const char *asValue)
+{
+	Cvar_Set(asName, asValue);
+};
+
+const char *CConVarController::GetString(const char *asName) const
+{
+	return Cvar_VariableString(asName);
+};
+
+void CConVarController::SetFloat(const char *asName, float afValue)
+{
+	Cvar_SetValue(asName, afValue);
+};
+
+float CConVarController::GetFloat(const char *asName) const
+{
+	return Cvar_VariableValue(asName);
+};
---
+++ mgt/engine/ConVarController.hpp
@@ -0,0 +1,37 @@
+/*
+ * This file is part of Magenta Engine
+ *
+ * Copyright (C) 2018 BlackPhrase
+ *
+ * Magenta Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Magenta Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+
+#pragma once
+
+#include "engine/IConVarController.hpp"
+
+class CConVarController final : public IConVarController
+{
+public:
+	CConVarController();
+	~CConVarController();
+	
+	void SetString(const char *asName, const char *asValue) override;
+	const char *GetString(const char *asName) const override;
+	
+	void SetFloat(const char *asName, float afValue) override;
+	float GetFloat(const char *asName) const override;
+};
