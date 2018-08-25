2304a996b53d51a8df834fc342303bfaf2d6077a
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-08-25 14:38:43 +0000

    [Engine] Add command processor interface implementation

---
+++ mgt/engine/CmdProcessor.cpp
@@ -0,0 +1,49 @@
+/*
+ * This file is part of Magenta Engine
+ *
+ * Copyright (C) 2018 BlackPhrase
+ *
+ * Magenta Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Magenta Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+
+#include "quakedef.h"
+#include "CmdProcessor.hpp"
+
+CCmdProcessor::CCmdProcessor() = default;
+CCmdProcessor::~CCmdProcessor() = default;
+
+void CCmdProcessor::QueCmd(const char *asText, InsertMode aeMode)
+{
+	switch(aeMode)
+	{
+	case InsertMode::Front:
+		Cbuf_InsertText(asText);
+		break;
+	case InsertMode::Back:
+		Cbuf_AddText(asText);
+		break;
+	};
+};
+
+void CCmdProcessor::ExecCmd(const char *asText)
+{
+	Cmd_ExecuteString(asText, src_command);
+};
+
+void CCmdProcessor::ExecQue()
+{
+	Cbuf_Execute();
+};
