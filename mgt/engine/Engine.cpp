09ed676997d04eb56446807af9192c8d5e89b8df
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-08-27 13:26:04 +0000

    [Engine] Update IEngine interface implementation, init filesystem and fill quakeparms values, apply some (temporary) fixes to make it work

--- mgt/engine/Engine.cpp
+++ mgt/engine/Engine.cpp
@@ -2,7 +2,7 @@
  * This file is part of Magenta Engine
  *
  * Copyright (C) 1996-1997 Id Software, Inc.
- * Copyright (C) 2018 BlackPhrase
+ * Copyright (C) 2017-2018 BlackPhrase
  *
  * Magenta Engine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -40,31 +40,87 @@ bool CEngine::Init(const SInitData &apInitParams)
 {
 	//static quakeparms_t parms; // TODO: static?
 
-	//memset(&parms, 0, sizeof(parms));
+	memset(&mParms, 0, sizeof(mParms));
 
-	//mParms. =;
+#if defined(GLQUAKE) || defined(SWDS)
+	mParms.memsize = 16 * 1024 * 1024;
+#else
+	mParms.memsize = 8 * 1024 * 1024; // TODO: 5861376 in QW
+#endif
+	
+	//COM_InitArgv(argc, argv); // TODO: parms.argc, parms.argv
+	
+	/*
+	if(COM_CheckParm("-heapsize"))
+	{
+		int t = COM_CheckParm("-heapsize") + 1;
 
-	//parms.memsize = 8*1024*1024; // TODO: 5861376 in QW
-	//parms.membase = malloc (parms.memsize);
-	//parms.basedir = ".";
+		if(t < com_argc)
+			parms.memsize = Q_atoi(com_argv[t]) * 1024;
+	};
+	*/
+	
+	mParms.membase = malloc(mParms.memsize);
+	
+	if(!mParms.membase)
+		Sys_Error("Not enough memory free; check disk space\n");
+	
+	// TODO: windows
+	/*
+	static char cwd[1024]{};
 
-	//COM_InitArgv (argc, argv); // TODO: parms.argc, parms.argv
+	_getcwd(cwd, sizeof(cwd));
+	
+	if(cwd[Q_strlen(cwd) - 1] == '\\')
+		cwd[Q_strlen(cwd) - 1] = 0;
+	*/
+	
+	mParms.basedir = (char*)apInitParams.sGameDir; //"."; // TODO: cwd
 
-	//parms.argc = com_argc;
-	//parms.argv = com_argv;
+	/*
+	// dedicated server ONLY!
+	if(!COM_CheckParm("-dedicated"))
+	{
+		memcpy(newargv, argv, argc * 4);
+		newargv[argc] = "-dedicated";
+		argc++;
+		argv = newargv;
+		COM_InitArgv(argc, argv);
+	}
+	*/
+	
+	//mParms.argc = com_argc;
+	//mParms.argv = com_argv;
 
 	//isDedicated = (COM_CheckParm ("-dedicated") != 0);
-
+	isDedicated = apInitParams.bDedicated;
+	
 	//Sys_InitGame(char *, char *, void *, int); // TODO
 
-	//printf("Host_Init\n");
+	//Sys_PageIn(parms.membase, parms.memsize); // TODO: windows
+	
+	//Sys_Init(); // TODO: windows
+	
+	FileSystem_Init(/*apInitParams.sGameDir*/mParms.basedir, (void*)apInitParams.fnLauncherFactory);
+
+	Q_strcpy(com_gamedir, apInitParams.sGameDir);
+	
+	// because sound is off until we become active
+	//S_BlockSound(); // TODO: windows
+	
+	//printf("Host_Init\n"); // TODO: Sys_Printf
 	Host_Init(&mParms);
+	
+	oldtime = Sys_FloatTime(); // TODO: ?
+	
 	return true;
 };
 
 void CEngine::Shutdown()
 {
 	Host_Shutdown();
+	
+	FileSystem_Shutdown();
 };
 
 bool CEngine::Frame()
@@ -79,12 +135,19 @@ bool CEngine::Frame()
 	
 	if(isDedicated)
 	{
-		while(frametime < sys_ticrate.value)
+		// TODO: can't get outta this loop
+		/*
+		while(frametime < sys_ticrate.GetValue())
 		{
-			Sys_Sleep(); // TODO: 1ms?
-			newtime = Sys_FloatTime();
-			frametime = newtime - oldtime;
+			Sys_Sleep(); // TODO: 1ms
+			// TODO
+			newtime = Sys_FloatTime(); // TODO: ???
+			frametime = newtime - oldtime; // TODO: ???
+			//
+			//continue; // TODO
+			//
 		};
+		*/
 	};
 	
 	Host_Frame(frametime);
