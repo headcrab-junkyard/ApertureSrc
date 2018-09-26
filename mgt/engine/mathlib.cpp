909673e8bc68ac30368208e2698b19ca051a4233
BlackPhrase <BlackPhrase@users.noreply.github.com>
2018-09-26 13:40:01 +0000

    Build mathlib as a static lib

--- mgt/engine/mathlib.cpp
+++ mgt/engine/mathlib.cpp
@@ -21,10 +21,9 @@
 /// @file
 /// @brief math primitives
 
-#include <math.h>
+#include <cmath>
 #include "quakedef.h"
 
-vec3_t vec3_origin = { 0, 0, 0 };
 int nanmask = 255 << 23;
 
 /*-----------------------------------------------------------------*/
@@ -150,17 +149,7 @@ void RotatePointAroundVector(vec3_t dst, const vec3_t dir, const vec3_t point, f
 
 /*-----------------------------------------------------------------*/
 
-float anglemod(float a)
-{
-#if 0
-	if (a >= 0)
-		a -= 360*(int)(a/360);
-	else
-		a += 360*( 1 + (int)(-a/360) );
-#endif
-	a = (360.0 / 65536) * ((int)(a * (65536 / 360.0)) & 65535);
-	return a;
-}
+//anglemod
 
 /*
 ==================
@@ -286,31 +275,7 @@ int BoxOnPlaneSide(vec3_t emins, vec3_t emaxs, mplane_t *p)
 
 //#endif !id386
 
-void AngleVectors(vec3_t angles, vec3_t forward, vec3_t right, vec3_t up)
-{
-	float angle;
-	float sr, sp, sy, cr, cp, cy;
-
-	angle = angles[YAW] * (M_PI * 2 / 360);
-	sy = sin(angle);
-	cy = cos(angle);
-	angle = angles[PITCH] * (M_PI * 2 / 360);
-	sp = sin(angle);
-	cp = cos(angle);
-	angle = angles[ROLL] * (M_PI * 2 / 360);
-	sr = sin(angle);
-	cr = cos(angle);
-
-	forward[0] = cp * cy;
-	forward[1] = cp * sy;
-	forward[2] = -sp;
-	right[0] = (-1 * sr * sp * cy + -1 * cr * -sy);
-	right[1] = (-1 * sr * sp * sy + -1 * cr * cy);
-	right[2] = -1 * sr * cp;
-	up[0] = (cr * sp * cy + -sr * -sy);
-	up[1] = (cr * sp * sy + -sr * cy);
-	up[2] = cr * cp;
-}
+//AngleVectors
 
 int VectorCompare(vec3_t v1, vec3_t v2)
 {
@@ -378,23 +343,7 @@ vec_t Length(vec3_t v)
 	return length;
 }
 
-float VectorNormalize(vec3_t v)
-{
-	float length, ilength;
-
-	length = v[0] * v[0] + v[1] * v[1] + v[2] * v[2];
-	length = sqrt(length); // FIXME
-
-	if(length)
-	{
-		ilength = 1 / length;
-		v[0] *= ilength;
-		v[1] *= ilength;
-		v[2] *= ilength;
-	}
-
-	return length;
-}
+//VectorNormalize
 
 void VectorInverse(vec3_t v)
 {
@@ -403,12 +352,7 @@ void VectorInverse(vec3_t v)
 	v[2] = -v[2];
 }
 
-void VectorScale(vec3_t in, vec_t scale, vec3_t out)
-{
-	out[0] = in[0] * scale;
-	out[1] = in[1] * scale;
-	out[2] = in[2] * scale;
-}
+//VectorScale
 
 int Q_log2(int val)
 {
---
+++ mgt/mathlib/mathlib.cpp
@@ -0,0 +1,99 @@
+/*
+*	This file is part of Magenta Engine
+*
+*	Copyright (C) 1996-1997 Id Software, Inc.
+*	Copyright (C) 2018 BlackPhrase
+*
+*	Magenta Engine is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*
+*	Magenta Engine is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License
+*	along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+/// @brief math primitives
+
+#include <cmath>
+#include "mathlib.h"
+
+// up / down
+#define PITCH 0
+
+// left / right
+#define YAW 1
+
+// fall over
+#define ROLL 2
+
+vec3_t vec3_origin = { 0, 0, 0 };
+
+float anglemod(float a)
+{
+#if 0
+	if (a >= 0)
+		a -= 360*(int)(a/360);
+	else
+		a += 360*( 1 + (int)(-a/360) );
+#endif
+	a = (360.0 / 65536) * ((int)(a * (65536 / 360.0)) & 65535);
+	return a;
+}
+
+void AngleVectors(vec3_t angles, vec3_t forward, vec3_t right, vec3_t up)
+{
+	float angle;
+	float sr, sp, sy, cr, cp, cy;
+
+	angle = angles[YAW] * (M_PI * 2 / 360);
+	sy = sin(angle);
+	cy = cos(angle);
+	angle = angles[PITCH] * (M_PI * 2 / 360);
+	sp = sin(angle);
+	cp = cos(angle);
+	angle = angles[ROLL] * (M_PI * 2 / 360);
+	sr = sin(angle);
+	cr = cos(angle);
+
+	forward[0] = cp * cy;
+	forward[1] = cp * sy;
+	forward[2] = -sp;
+	right[0] = (-1 * sr * sp * cy + -1 * cr * -sy);
+	right[1] = (-1 * sr * sp * sy + -1 * cr * cy);
+	right[2] = -1 * sr * cp;
+	up[0] = (cr * sp * cy + -sr * -sy);
+	up[1] = (cr * sp * sy + -sr * cy);
+	up[2] = cr * cp;
+}
+
+float VectorNormalize(vec3_t v)
+{
+	float length, ilength;
+
+	length = v[0] * v[0] + v[1] * v[1] + v[2] * v[2];
+	length = sqrt(length); // FIXME
+
+	if(length)
+	{
+		ilength = 1 / length;
+		v[0] *= ilength;
+		v[1] *= ilength;
+		v[2] *= ilength;
+	}
+
+	return length;
+}
+
+void VectorScale(vec3_t in, vec_t scale, vec3_t out)
+{
+	out[0] = in[0] * scale;
+	out[1] = in[1] * scale;
+	out[2] = in[2] * scale;
+}
