9005a3167e15b595a3bd4709c6f7915a9bb487fc
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-03-13 13:27:09 +0000

    [EngineClient] Client game (wrapper) code is fixed (?)

--- mgt/engineclient/ClientGame.cpp
+++ mgt/engineclient/ClientGame.cpp
@@ -1,20 +1,20 @@
 /*
-*	This file is part of Magenta Engine
-*
-*	Copyright (C) 2018 BlackPhrase
-*
-*	Magenta Engine is free software: you can redistribute it and/or modify
-*	it under the terms of the GNU General Public License as published by
-*	the Free Software Foundation, either version 3 of the License, or
-*	(at your option) any later version.
-*
-*	Magenta Engine is distributed in the hope that it will be useful,
-*	but WITHOUT ANY WARRANTY; without even the implied warranty of
-*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-*	GNU General Public License for more details.
-*
-*	You should have received a copy of the GNU General Public License
-*	along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+ * This file is part of Magenta Engine
+ *
+ * Copyright (C) 2018 BlackPhrase
+ *
+ * Magenta Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Magenta Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
 */
 
 /// @file
@@ -38,7 +38,7 @@ void CClientGame::Init()
 {
 	// TODO
 
-	ClientGame_Shutdown();
+	Shutdown();
 
 #ifndef MGT_CLIENTGAME_HARD_LINKED
 	if(!LoadModule())
@@ -52,8 +52,8 @@ void CClientGame::Init()
 
 void CClientGame::Shutdown()
 {
-	//if(mpGame)
-		//mpGame->Shutdown();
+	if(mpGame)
+		mpGame->Shutdown();
 
 #ifndef MGT_CLIENTGAME_HARD_LINKED
 	UnloadModule();
