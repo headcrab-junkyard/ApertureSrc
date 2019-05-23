d74b58c64fdc5d9c00e44ccec0f663c51c5a3282
BlackPhrase <BlackPhrase@users.noreply.github.com>
2019-05-23 13:15:00 +0000

    [MathLib] Move more functions (required by pm_shared code) here from engine

--- mgt/mathlib/mathlib.cpp
+++ mgt/mathlib/mathlib.cpp
@@ -2,7 +2,7 @@
 *	This file is part of Magenta Engine
 *
 *	Copyright (C) 1996-1997 Id Software, Inc.
-*	Copyright (C) 2018 BlackPhrase
+*	Copyright (C) 2018-2019 BlackPhrase
 *
 *	Magenta Engine is free software: you can redistribute it and/or modify
 *	it under the terms of the GNU General Public License as published by
@@ -22,18 +22,13 @@
 /// @brief math primitives
 
 #include <cmath>
-#include "mathlib.h"
+#include "mathlib/mathlib.h"
 
-// up / down
-#define PITCH 0
+#define PITCH 0 // up / down
+#define YAW 1 // left / right
+#define ROLL 2 // fall over
 
-// left / right
-#define YAW 1
-
-// fall over
-#define ROLL 2
-
-vec3_t vec3_origin = { 0, 0, 0 };
+vec3_t vec3_origin = { 0.0f, 0.0f, 0.0f };
 
 float anglemod(float a)
 {
@@ -47,7 +42,7 @@ float anglemod(float a)
 	return a;
 }
 
-void AngleVectors(vec3_t angles, vec3_t forward, vec3_t right, vec3_t up)
+void AngleVectors(const vec3_t angles, vec3_t forward, vec3_t right, vec3_t up)
 {
 	float angle;
 	float sr, sp, sy, cr, cp, cy;
@@ -73,6 +68,72 @@ void AngleVectors(vec3_t angles, vec3_t forward, vec3_t right, vec3_t up)
 	up[2] = cr * cp;
 }
 
+int VectorCompare(vec3_t v1, vec3_t v2)
+{
+	int i;
+
+	for(i = 0; i < 3; i++)
+		if(v1[i] != v2[i])
+			return 0;
+
+	return 1;
+}
+
+void VectorMA(vec3_t veca, float scale, vec3_t vecb, vec3_t vecc)
+{
+	vecc[0] = veca[0] + scale * vecb[0];
+	vecc[1] = veca[1] + scale * vecb[1];
+	vecc[2] = veca[2] + scale * vecb[2];
+}
+
+vec_t _DotProduct(vec3_t v1, vec3_t v2)
+{
+	return v1[0] * v2[0] + v1[1] * v2[1] + v1[2] * v2[2];
+}
+
+void _VectorSubtract(vec3_t veca, vec3_t vecb, vec3_t out)
+{
+	out[0] = veca[0] - vecb[0];
+	out[1] = veca[1] - vecb[1];
+	out[2] = veca[2] - vecb[2];
+}
+
+void _VectorAdd(vec3_t veca, vec3_t vecb, vec3_t out)
+{
+	out[0] = veca[0] + vecb[0];
+	out[1] = veca[1] + vecb[1];
+	out[2] = veca[2] + vecb[2];
+}
+
+void _VectorCopy(vec3_t in, vec3_t out)
+{
+	out[0] = in[0];
+	out[1] = in[1];
+	out[2] = in[2];
+}
+
+void CrossProduct(vec3_t v1, vec3_t v2, vec3_t cross)
+{
+	cross[0] = v1[1] * v2[2] - v1[2] * v2[1];
+	cross[1] = v1[2] * v2[0] - v1[0] * v2[2];
+	cross[2] = v1[0] * v2[1] - v1[1] * v2[0];
+}
+
+double sqrt(double x);
+
+vec_t Length(vec3_t v)
+{
+	int i;
+	float length;
+
+	length = 0;
+	for(i = 0; i < 3; i++)
+		length += v[i] * v[i];
+	length = sqrt(length); // FIXME
+
+	return length;
+}
+
 float VectorNormalize(vec3_t v)
 {
 	float length, ilength;
@@ -91,9 +152,24 @@ float VectorNormalize(vec3_t v)
 	return length;
 }
 
+void VectorInverse(vec3_t v)
+{
+	v[0] = -v[0];
+	v[1] = -v[1];
+	v[2] = -v[2];
+}
+
 void VectorScale(vec3_t in, vec_t scale, vec3_t out)
 {
 	out[0] = in[0] * scale;
 	out[1] = in[1] * scale;
 	out[2] = in[2] * scale;
+}
+
+int Q_log2(int val)
+{
+	int answer = 0;
+	while(val >>= 1) // TODO:  while((val>>=1) != 0)
+		answer++;
+	return answer;
 }
