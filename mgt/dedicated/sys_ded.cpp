75e90e6b3477a8c76b5643605cc8637ecb67eb17
BlackPhrase <BlackPhrase@users.noreply.github.com>
2020-04-10 14:05:02 +0000

    [MGTDS] Update license headers

--- mgt/dedicated/sys_ded.cpp
+++ mgt/dedicated/sys_ded.cpp
@@ -1,20 +1,20 @@
 /*
-*	This file is part of Magenta Engine
+*	This file is part of OGSNext Engine
 *
-*	Copyright (C) 2018-2019 BlackPhrase
+*	Copyright (C) 2018-2020 BlackPhrase
 *
-*	Magenta Engine is free software: you can redistribute it and/or modify
+*	OGSNext Engine is free software: you can redistribute it and/or modify
 *	it under the terms of the GNU General Public License as published by
 *	the Free Software Foundation, either version 3 of the License, or
 *	(at your option) any later version.
 *
-*	Magenta Engine is distributed in the hope that it will be useful,
+*	OGSNext Engine is distributed in the hope that it will be useful,
 *	but WITHOUT ANY WARRANTY; without even the implied warranty of
 *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 *	GNU General Public License for more details.
 *
 *	You should have received a copy of the GNU General Public License
-*	along with Magenta Engine. If not, see <http://www.gnu.org/licenses/>.
+*	along with OGSNext Engine. If not, see <http://www.gnu.org/licenses/>.
 */
 
 /// @file
