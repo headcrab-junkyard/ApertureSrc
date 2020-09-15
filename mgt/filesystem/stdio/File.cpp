19a16e438bc31a713fff8155ffa47334182bc23d
BlackPhrase <BlackPhrase@users.noreply.github.com>
2020-09-15 14:20:05 +0000

    [FileSystem] Update license headers (year, rebranding)

--- mgt/filesystem/stdio/File.cpp
+++ mgt/filesystem/stdio/File.cpp
@@ -1,20 +1,21 @@
 /*
- *	This file is part of Magenta Engine
- *	Copyright (C) 2018 BlackPhrase
+ * This file is part of OGSNext Engine
  *
- *	This program is free software: you can redistribute it and/or modify
- *	it under the terms of the GNU General Public License as published by
- *	the Free Software Foundation, either version 3 of the License, or
- *	(at your option) any later version.
+ * Copyright (C) 2018, 2020 BlackPhrase
  *
- *	This program is distributed in the hope that it will be useful,
- *	but WITHOUT ANY WARRANTY; without even the implied warranty of
- *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *	GNU General Public License for more details.
+ * OGSNext Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *	You should have received a copy of the GNU General Public License
- *	along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+ * OGSNext Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OGSNext Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 /// @file
 
