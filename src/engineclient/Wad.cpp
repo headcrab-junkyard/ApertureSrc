e21879220a9cbc532fa9c5e545b6ab7ec7107fcd
BlackPhrase <BlackPhrase@users.noreply.github.com>
2021-02-04 22:49:39 +0000

    [src/EngineClient] CWAD -> CWADCache

--- src/engineclient/Wad.cpp
+++ src/engineclient/Wad.cpp
@@ -1,27 +1,28 @@
 #include "quakedef.h"
+
 #include "Wad.hpp"
 
-void CWAD::LoadWadFile(const char *filename)
+void CWADCache::LoadWadFile(const char *filename)
 {
 	W_LoadWadFile(filename);
 };
 
-void CWAD::CleanupName(const char *in, char *out)
+void CWADCache::CleanupName(const char *in, char *out)
 {
 	W_CleanupName(in, out);
 };
 
-lumpinfo_t *CWAD::GetLumpInfo(const char *name)
+lumpinfo_t *CWADCache::GetLumpInfo(const char *name)
 {
 	return W_GetLumpinfo(name);
 };
 
-void *CWAD::GetLumpName(const char *name)
+void *CWADCache::GetLumpName(const char *name)
 {
 	return W_GetLumpName(name);
 };
 
-void *CWAD::GetLumpNum(int num)
+void *CWADCache::GetLumpNum(int num)
 {
 	return W_GetLumpNum(num);
 };
