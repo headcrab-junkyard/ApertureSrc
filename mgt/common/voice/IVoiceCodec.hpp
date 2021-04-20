17633e87e9dd91f6cd2cb2182b008bb565ade135
BlackPhrase <BlackPhrase@users.noreply.github.com>
2021-04-20 13:38:28 +0000

    [Common] Update the license headers (year, rebranding)

--- mgt/common/voice/IVoiceCodec.hpp
+++ mgt/common/voice/IVoiceCodec.hpp
@@ -1,19 +1,20 @@
 /*
- * This file is part of OGS Engine
- * Copyright (C) 2019 BlackPhrase
+ * This file is part of OGSNext Engine
  *
- * OGS Engine is free software: you can redistribute it and/or modify
+ * Copyright (C) 2019-2020 BlackPhrase
+ *
+ * OGSNext Engine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * OGS Engine is distributed in the hope that it will be useful,
+ * OGSNext Engine is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with OGS Engine. If not, see <http://www.gnu.org/licenses/>.
+ * along with OGSNext Engine. If not, see <http://www.gnu.org/licenses/>.
  */
 
 /// @file
