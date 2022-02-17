1fd9d629e4274e4decfd100e470106ce44cfdfe8
BlackPhrase <BlackPhrase@users.noreply.github.com>
2022-02-17 13:50:45 +0000

    [src/Engine] Some initial code pieces to support engine states

--- src/engine/Engine.hpp
+++ src/engine/Engine.hpp
@@ -1,7 +1,7 @@
 /*
  * This file is part of OGSNext Engine
  *
- * Copyright (C) 2020 BlackPhrase
+ * Copyright (C) 2020, 2022 BlackPhrase
  *
  * OGSNext Engine is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,12 +22,17 @@
 #pragma once
 
 #include <memory>
+#include <vector>
 
 #include "engine/IEngine.hpp"
 
 class CHost;
 
+interface IEngineState;
 interface IEngineClient;
+
+
+using tEngineStateVec = std::vector<IEngineState*>;
 
 class CEngine final : public IEngine
 {
@@ -37,10 +42,30 @@ public:
 	
 	bool Frame() override;
 private:
+	enum class State
+	{
+		Inactive,
+		Active,
+		Paused,
+		Close,
+		LevelTransition
+	};
+
+	void SetState(State aeState)
+	{
+		mvStates[meActiveState]->OnLeave();
+		meActiveState = aeState;
+		mvStates[meActiveState]->OnEnter();
+	};
+	State GetState() const {return meActiveState;}
 	IEngineClient *LoadClientModule();
 	void UnloadClientModule();
 private:
 	std::unique_ptr<CHost> mpHost;
 	
 	void *mpClientModule{nullptr};
+
+	tEngineStateVec mvStates;
+
+	State meActiveState{0};
 };
