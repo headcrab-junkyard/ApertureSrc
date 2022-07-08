becb4302b06dd9a951ac7035f1f7ddf0ef6648bc
BlackPhrase <BlackPhrase@users.noreply.github.com>
2022-07-08 17:05:36 +0000

    [src/FileSystem] Legacy fs interface impl

---
+++ src/filesystem/stdio/FileSystemLegacy.cpp
@@ -0,0 +1,350 @@
+/*
+ * This file is part of OGSNext Engine
+ *
+ * Copyright (C) 2020-2022 BlackPhrase
+ *
+ * OGSNext Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OGSNext Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OGSNext Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+
+#include "FileSystemLegacy.hpp"
+#include "FileSystem.hpp"
+
+extern CFileSystem *gpFileSystem;
+
+CFileSystemLegacy::CFileSystemLegacy() = default;
+CFileSystemLegacy::~CFileSystemLegacy() = default;
+
+void CFileSystemLegacy::Mount()
+{
+	gpFileSystem->Mount();
+};
+
+void CFileSystemLegacy::Unmount()
+{
+	gpFileSystem->Unmount();
+};
+
+void CFileSystemLegacy::RemoveAllSearchPaths()
+{
+	gpFileSystem->RemoveAllSearchPaths();
+};
+
+void CFileSystemLegacy::AddSearchPath(const char *asPath, const char *asPathID)
+{
+	gpFileSystem->AddSearchPath(asPath, asPathID, false);
+};
+
+bool CFileSystemLegacy::RemoveSearchPath(const char *asPath)
+{
+	return gpFileSystem->RemoveSearchPath(asPath);
+};
+
+void CFileSystemLegacy::RemoveFile(const char *asRelativePath, const char *asPathID)
+{
+	// TODO
+	//gpFileSystem->RemoveFile(asRelativePath, asPathID);
+};
+
+void CFileSystemLegacy::CreateDirHierarchy(const char *asPath, const char *asPathID)
+{
+	// TODO
+	//gpFileSystem->CreateDirHierarchy(asPath, asPathID);
+};
+
+bool CFileSystemLegacy::FileExists(const char *asFileName)
+{
+	return gpFileSystem->IsFileExists(asFileName);
+};
+
+bool CFileSystemLegacy::IsDirectory(const char *asFileName)
+{
+	return gpFileSystem->IsDirectory(asFileName);
+};
+
+FileHandle_t CFileSystemLegacy::Open(const char *asFileName, const char *asOptions, const char *asPathID)
+{
+	return reinterpret_cast<FileHandle_t>(gpFileSystem->OpenFile(asFileName, asOptions, asPathID));
+};
+
+void CFileSystemLegacy::Close(FileHandle_t anHandle)
+{
+	gpFileSystem->CloseFile(ToFile(anHandle));
+};
+
+void CFileSystemLegacy::Seek(FileHandle_t anHandle, int anPos, FileSystemSeek_t aeSeekType)
+{
+	ToFile(anHandle)->Seek(anPos, aeSeekType);
+};
+
+unsigned int CFileSystemLegacy::Tell(FileHandle_t anHandle)
+{
+	return ToFile(anHandle)->Tell();
+};
+
+unsigned int CFileSystemLegacy::Size(FileHandle_t anHandle)
+{
+	return ToFile(anHandle)->GetSize();
+};
+
+unsigned int CFileSystemLegacy::Size(const char *asFileName)
+{
+	return gpFileSystem->GetFileSize(asFileName);
+};
+
+long CFileSystemLegacy::GetFileTime(const char *asFileName)
+{
+	return gpFileSystem->GetFileTime(asFileName);
+};
+
+void CFileSystemLegacy::FileTimeToString(char *asStrip, int anMaxCharsIncludingTerminator, long anFileTime)
+{
+	// TODO
+	//gpFileSystem->FileTimeToString(asStrip, anMaxCharsIncludingTerminator, anFileTime);
+};
+
+bool CFileSystemLegacy::IsOk(FileHandle_t anHandle)
+{
+	return ToFile(anHandle)->IsOk();
+};
+
+void CFileSystemLegacy::Flush(FileHandle_t anHandle)
+{
+	ToFile(anHandle)->Flush();
+};
+
+bool CFileSystemLegacy::EndOfFile(FileHandle_t anHandle)
+{
+	return ToFile(anHandle)->IsEOF();
+};
+
+int CFileSystemLegacy::Read(void *apOutput, int anSize, FileHandle_t anHandle)
+{
+	return ToFile(anHandle)->Read(apOutput, anSize);
+};
+
+int CFileSystemLegacy::Write(const void *apInput, int anSize, FileHandle_t anHandle) // void const
+{
+	return ToFile(anHandle)->Write(apInput, anSize);
+};
+
+char *CFileSystemLegacy::ReadLine(char *asOutput, int anMaxChars, FileHandle_t anHandle)
+{
+	// TODO
+	//return ToFile(anHandle)->ReadLine(asOutput, anMaxChars);
+	return nullptr;
+};
+
+int CFileSystemLegacy::FPrintf(FileHandle_t anHandle, char *asFormat, ...)
+{
+	// TODO
+	return ToFile(anHandle)->Printf(asFormat);
+};
+
+void *CFileSystemLegacy::GetReadBuffer(FileHandle_t anHandle, int *anOutBufferSize, bool abFailIfNotInCache)
+{
+	// TODO
+	//return ToFile(anHandle)->GetReadBuffer(anOutBufferSize, abFailIfNotInCache);
+	return nullptr;
+};
+
+void CFileSystemLegacy::ReleaseReadBuffer(FileHandle_t anHandle, void *apBuffer)
+{
+	// TODO
+	//ToFile(anHandle)->ReleaseReadBuffer(apBuffer);
+};
+
+const char *CFileSystemLegacy::FindFirst(const char *asWildCard, FileFindHandle_t *apHandle, const char *asPathID)
+{
+	// TODO
+	//return gpFileSystem->FindFirst(asWildCard, apHandle, asPathID);
+	return "";
+};
+
+const char *CFileSystemLegacy::FindNext(FileFindHandle_t anHandle)
+{
+	// TODO
+	//return gpFileSystem->FindNext(anHandle);
+	return "";
+};
+
+bool CFileSystemLegacy::FindIsDirectory(FileFindHandle_t anHandle)
+{
+	// TODO
+	//return gpFileSystem->FindIsDirectory(anHandle);
+	return false;
+};
+
+void CFileSystemLegacy::FindClose(FileFindHandle_t anHandle)
+{
+	// TODO
+	//gpFileSystem->FindClose(anHandle);
+};
+
+void CFileSystemLegacy::GetLocalCopy(const char *asFileName)
+{
+	// TODO
+	//gpFileSystem->GetLocalCopy(asFileName);
+};
+
+const char *CFileSystemLegacy::GetLocalPath(const char *asFileName, char *asLocalPath, int anBufferSize)
+{
+	// TODO
+	//return gpFileSystem->GetLocalPath(asFileName, asLocalPath, anBufferSize);
+	return "";
+};
+
+char *CFileSystemLegacy::ParseFile(char *pFileBytes, char *pToken, bool *pWasQuoted)
+{
+	// TODO
+	//return gpFileSystem->ParseFile();
+	return nullptr;
+};
+
+bool CFileSystemLegacy::FullPathToRelativePath(const char *asFullPath, char *asRelativePath)
+{
+	// TODO
+	//return gpFileSystem->FullPathToRelativePath(asFullPath, asRelativePath);
+	return false;
+};
+
+bool CFileSystemLegacy::GetCurrentDirectory(char *asDirectory, int anMaxLen)
+{
+	// TODO
+	//return gpFileSystem->GetCurrentDirectory(asDirectory, anMaxLen);
+	return false;
+};
+
+void CFileSystemLegacy::PrintOpenedFiles()
+{
+	// TODO
+	//gpFileSystem->PrintOpenedFiles();
+};
+
+void CFileSystemLegacy::SetWarningFunc(void (*pfnWarning)(const char *fmt, ...))
+{
+	// TODO
+	//gpFileSystem->SetWarningFunc(pfnWarning);
+};
+
+void CFileSystemLegacy::SetWarningLevel(FileWarningLevel_t aeLevel)
+{
+	// TODO
+	//gpFileSystem->SetWarningLevel(aeLevel);
+};
+
+void CFileSystemLegacy::LogLevelLoadStarted(const char *asName)
+{
+	// TODO
+	//gpFileSystem->LogLevelLoadStarted(asName);
+};
+
+void CFileSystemLegacy::LogLevelLoadFinished(const char *asName)
+{
+	// TODO
+	//gpFileSystem->LogLevelLoadFinished(asName);
+};
+
+int CFileSystemLegacy::HintResourceNeed(const char *asHintList, int anForgetEverything)
+{
+	// TODO
+	//return gpFileSystem->HintResourceNeed(asHintList, anForgetEverything);
+	return 0;
+};
+
+int CFileSystemLegacy::PauseResourcePreloading()
+{
+	// TODO
+	//return gpFileSystem->PauseResourcePreloading();
+	return 0;
+};
+
+int CFileSystemLegacy::ResumeResourcePreloading()
+{
+	// TODO
+	//return gpFileSystem->ResumeResourcePreloading();
+	return 0;
+};
+
+int CFileSystemLegacy::SetVBuf(FileHandle_t anHandle, char *apBuffer, int anMode, long anSize)
+{
+	return ToFile(anHandle)->SetVBuf(apBuffer, anMode, anSize);
+};
+
+void CFileSystemLegacy::GetInterfaceVersion(char *p, int maxlen)
+{
+	// TODO
+	//gpFileSystem->GetInterfaceVersion(p, maxlen);
+};
+
+bool CFileSystemLegacy::IsFileImmediatelyAvailable(const char *sFileName)
+{
+	// TODO
+	//return gpFileSystem->IsFileImmediatelyAvailable(sFileName);
+	return false;
+};
+
+WaitForResourcesHandle_t CFileSystemLegacy::WaitForResources(const char *sResourceList)
+{
+	// TODO
+	//return gpFileSystem->WaitForResources(sResourceList);
+	return 0;
+};
+
+bool CFileSystemLegacy::GetWaitForResourcesProgress(WaitForResourcesHandle_t handle,
+								 float *progress /* out */,
+								 bool *complete /* out */)
+{
+	// TODO
+	//return gpFileSystem->GetWaitForResourcesProgress(progress, complete);
+	return false;
+};
+
+void CFileSystemLegacy::CancelWaitForResources(WaitForResourcesHandle_t handle)
+{
+	// TODO
+	//gpFileSystem->CancelWaitForResources(handle);
+};
+
+bool CFileSystemLegacy::IsAppReadyForOfflinePlay(int nAppID)
+{
+	// TODO
+	//return gpFileSystem->IsAppReadyForOfflinePlay(nAppID);
+	return false;
+};
+
+bool CFileSystemLegacy::AddPackFile(const char *sFullPath, const char *sPathID)
+{
+	// TODO
+	//return gpFileSystem->AddPackFile(sFullPath, sPathID);
+	return false;
+};
+
+FileHandle_t CFileSystemLegacy::OpenFromCacheForRead(const char *asFileName, const char *asOptions, const char *asPathID)
+{
+	// TODO
+	//return reinterpret_cast<FileHandle_t>(gpFileSystem->OpenFromCacheForRead(asFileName, asOptions, asPathID));
+	return 0;
+};
+
+void CFileSystemLegacy::AddSearchPathNoWrite(const char *asPath, const char *asPathID)
+{
+	gpFileSystem->AddSearchPath(asPath, asPathID, true);
+};
+
+IFile *CFileSystemLegacy::ToFile(FileHandle_t anFileHandle) const
+{
+	return reinterpret_cast<IFile*>(anFileHandle);
+};
---
+++ src/filesystem/stdio/FileSystemLegacy.hpp
@@ -0,0 +1,169 @@
+/*
+ * This file is part of OGSNext Engine
+ *
+ * Copyright (C) 1996-2001 Id Software, Inc.
+ * Copyright (C) 2016-2022 BlackPhrase
+ *
+ * OGSNext Engine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OGSNext Engine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OGSNext Engine. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/// @file
+/// @brief IFileSystem interface implementation
+
+#pragma once
+
+#include <cstdio>
+
+#include <public/FileSystem.h>
+
+// directory searching
+#define SFF_ARCH    0x01
+#define SFF_HIDDEN  0x02
+#define SFF_RDONLY  0x04
+#define SFF_SUBDIR  0x08
+#define SFF_SYSTEM  0x10
+
+#define MAX_OSPATH 128 // max length of a filesystem pathname
+
+typedef struct pack_s pack_t;
+
+/*
+** pass in an attribute mask of things you wish to REJECT
+*/
+char *Sys_FindFirst(const char *path, unsigned int musthave, unsigned int canthave);
+char *Sys_FindNext(unsigned int musthave, unsigned int canthave);
+void Sys_FindClose();
+
+// TODO: CFileSystem009?
+class CFileSystemLegacy final : public IFileSystem
+{
+public:
+	CFileSystemLegacy();
+	~CFileSystemLegacy();
+
+	void Mount() override;
+	void Unmount() override;
+
+	void RemoveAllSearchPaths() override;
+
+	void AddSearchPath(const char *pPath, const char *pathID) override;
+	bool RemoveSearchPath(const char *pPath) override;
+
+	void RemoveFile(const char *pRelativePath, const char *pathID) override;
+
+	void CreateDirHierarchy(const char *path, const char *pathID) override;
+
+	bool FileExists(const char *pFileName) override;
+	bool IsDirectory(const char *sFileName) override;
+
+	FileHandle_t Open(const char *sFileName, const char *pOptions, const char *pathID) override;
+	void Close(FileHandle_t file) override;
+
+	void Seek(FileHandle_t file, int pos, FileSystemSeek_t seekType) override;
+	unsigned int Tell(FileHandle_t file) override;
+
+	unsigned int Size(FileHandle_t file) override;
+	unsigned int Size(const char *sFileName) override;
+
+	long GetFileTime(const char *sFileName) override;
+	void FileTimeToString(char *pStrip, int maxCharsIncludingTerminator, long fileTime) override;
+
+	bool IsOk(FileHandle_t file) override;
+
+	void Flush(FileHandle_t file) override;
+	bool EndOfFile(FileHandle_t file) override;
+
+	int Read(void *pOutput, int size, FileHandle_t file) override;
+	int Write(void const *pInput, int size, FileHandle_t file) override; // void const
+	char *ReadLine(char *pOutput, int maxChars, FileHandle_t file) override;
+	int FPrintf(FileHandle_t file, char *pFormat, ...) override;
+
+	void *GetReadBuffer(FileHandle_t file, int *outBufferSize, bool failIfNotInCache) override;
+	void ReleaseReadBuffer(FileHandle_t file, void *readBuffer) override;
+
+	const char *FindFirst(const char *sWildCard, FileFindHandle_t *pHandle, const char *sPathID) override;
+	const char *FindNext(FileFindHandle_t handle) override;
+	
+	bool FindIsDirectory(FileFindHandle_t handle) override;
+	void FindClose(FileFindHandle_t handle) override;
+
+	void GetLocalCopy(const char *sFileName) override;
+
+	const char *GetLocalPath(const char *sFileName, char *pLocalPath, int localPathBufferSize) override;
+
+	char *ParseFile(char *pFileBytes, char *pToken, bool *pWasQuoted) override;
+
+	bool FullPathToRelativePath(const char *sFullpath, char *pRelative) override;
+
+	bool GetCurrentDirectory(char *sDirectory, int maxlen) override;
+
+	void PrintOpenedFiles() override;
+
+	void SetWarningFunc(void (*pfnWarning)(const char *fmt, ...)) override;
+	void SetWarningLevel(FileWarningLevel_t level) override;
+
+	void LogLevelLoadStarted(const char *sName) override;
+	void LogLevelLoadFinished(const char *sName) override;
+	
+	int HintResourceNeed(const char *sHintList, int nForgetEverything) override;
+	
+	int PauseResourcePreloading() override;
+	int ResumeResourcePreloading() override;
+	
+	int SetVBuf(FileHandle_t stream, char *buffer, int mode, long size) override;
+	
+	void GetInterfaceVersion(char *p, int maxlen) override;
+	
+	bool IsFileImmediatelyAvailable(const char *sFileName) override;
+
+	WaitForResourcesHandle_t WaitForResources(const char *sResourceList) override;
+	bool GetWaitForResourcesProgress(WaitForResourcesHandle_t handle,
+	                                 float *progress /* out */,
+	                                 bool *complete /* out */) override;
+	void CancelWaitForResources(WaitForResourcesHandle_t handle) override;
+
+	bool IsAppReadyForOfflinePlay(int nAppID) override;
+
+	bool AddPackFile(const char *sFullPath, const char *sPathID) override;
+
+	FileHandle_t OpenFromCacheForRead(const char *sFileName, const char *sOptions, const char *sPathID) override;
+
+	void AddSearchPathNoWrite(const char *sPath, const char *sPathID) override;
+private:
+	IFile *ToFile(FileHandle_t anFileHandle) const;
+private:
+	static constexpr auto MAX_HANDLES{10};
+	
+	FileHandle_t FindFile(const char *filename);
+	
+	pack_t *LoadPackFile(const char *packfile);
+	void AddGameDirectory(const char *dir);
+	
+	int findhandle() const;
+	int filelength(FILE *f);
+	
+#ifdef _WIN32
+	//FILE sys_handles[MAX_HANDLES]{};
+#elif sun
+	struct MEMFILE
+	{
+		FILE *hFile{nullptr};
+		char *pMap{nullptr};
+		int nLen{0};
+		int nPos{0};
+	};
+
+	//MEMFILE sys_handles[MAX_HANDLES]{};
+#endif
+};
